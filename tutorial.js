d66eb922befd191e9210ecf9e79ae6852239021f
Jordan Rickman <jordan.rickman.42@gmail.com>
2017-02-23 13:20:03 -0800

    Re-structured module with separate files for each class. Also made minimal runnable tutorial.

---
+++ tutorial.js
@@ -0,0 +1,16 @@
+//# Tutorial
+
+// Modules required for testing
+const assert = require('assert');
+
+// Using relative path because this code lives in the project; you should instead `require('dataflow')`.
+const dataflow = require('./lib');
+
+// Simple happy-path test
+const x = dataflow(2);
+const y = new dataflow.DependentNode(function (x) { return x*x; }, [x]);
+assert(x.value() === 2);
+assert(y.value() === 4);
+x.update(3);
+assert(x.value() === 3);
+assert(y.value() === 9);
