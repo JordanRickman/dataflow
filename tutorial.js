64b82f61273d2d644536abe2f7c23bb5ce27f79e
Jordan Rickman <jordan.rickman.42@gmail.com>
2017-04-03 21:20:30 -0700

    Implemented map() method for all nodes.

--- tutorial.js
+++ tutorial.js
@@ -27,4 +27,22 @@ assert.throws(
   }, /not implemented/
 );
 
+const z = dataflow([1, 2, 3]);
+const w = z.map((x) => Math.pow(2, x));
+//assert(z.value() === [1, 2, 3]); TODO need assertion library with deepEquals
+//assert(w.value() === [2, 4, 8]);
+console.log(`z: ${z.value()}`);
+console.log(`w: ${w.value()}`);
+
+const s = dataflow("hello");
+const S = s.map((c, i) => c.toUpperCase() + i);
+assert(s.value() === "hello");
+console.log(`S: ${S.value()}`);
+//assert(S.value() === ["H0","E1","L2","L3","O4"]);
+
+const o = dataflow({ key1: "val1", key2: "val2" });
+const O = o.map((v, k) => `${k}: ${v}`);
+console.log(`O: ${O.value()}`);
+//assert(O.value() === ['key1: val1', 'key2: val2']);
+
 console.log("All tests passed.");
